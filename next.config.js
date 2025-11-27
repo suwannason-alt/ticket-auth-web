d7f6f6a75da58f0f03323c496d518b69bf1f0017
suwannason <suwannss@ais.co.th>
2025-11-27 10:26:53 +0700

    refactor: 💡 authweb

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const createNextIntlPlugin = require('next-intl/plugin');
  
 const withNextIntl = createNextIntlPlugin();
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/admin/:path*',
-        destination: 'http://localhost:3040/admin/:path*',
-      },
-
-      {
-        source: '/ticket/:path*',
-        destination: 'http://localhost:4001/ticket/:path*',
-      },
-    ]
-  },
-};
+const nextConfig = {};
 
 module.exports = withNextIntl(nextConfig);
